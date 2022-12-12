d97046b11d090b485e8d47f1f64bd5f90e7221f3
Rahul <rahul10kr7@gmail.com>
2022-12-12 17:51:37 +0530

    redux part start

--- todoapp/src/Components/Home.jsx
+++ todoapp/src/Components/Home.jsx
@@ -2,15 +2,18 @@ import { Button } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Todo from "./Todo";
+import { useState } from "react";
 
 const Home = () => {
+    const [data, setData] = useState('')
+    console.log("data",data)
   return (
     <div>
       <div className="container">
         <section className="mt-3 text-center">
           <h3>Enter Your task</h3>
           <div className="todo col-lg-5 mx-auto d-flex justify-content-between align-items-center">
-            <input className="form-control" />
+            <input name="task" value={data} className="form-control" onChange={(e)=>setData(e.target.value)} />
             <Button
               variant="contained"
               className="mx-3"
--- todoapp/src/Components/Todo.jsx
+++ todoapp/src/Components/Todo.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 const Todo = () => {
   return (
     <div>
-      Todo-List
+    <div className="todo_data col-lg-5 mx-auto mt-2">
+        <div className="todo_container d-flex justify-content-between align-items-center px-2" style={{background:"#dcdde1", borderRadius:"3px", height:"45px"}}>
+            <li style={{ listStyle:"none"}}>ReachJs</li>
+            <div className="edit_dlt col-lg-3 py-2 d-flex justify-content-between align-items-center">
+                <ModeEditIcon style={{color:"#337ab7 ", cursor:"pointer"}}/>
+                <DeleteForeverIcon style={{color:"red ", cursor:"pointer"}}/>
+                <RemoveRedEyeIcon style={{color:"#00d290", cursor:"pointer"}}/>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
